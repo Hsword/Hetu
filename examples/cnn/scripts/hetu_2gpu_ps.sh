a39f10f531c08d9ac680f659efb7dc0611f19d01
HugoZHL <657671989@qq.com>
2022-04-03 12:33:51 +0000

    improve cnn scripts

--- examples/cnn/scripts/hetu_2gpu_ps.sh
+++ examples/cnn/scripts/hetu_2gpu_ps.sh
@@ -4,7 +4,4 @@ workdir=$(cd $(dirname $0); pwd)
 mainpy=${workdir}/../main.py
 
 ### validate and timing
-python -m hetu.launcher ${workdir}/../local_s1.yml -n 1 --sched &
-python ${mainpy} --model $1 --dataset $2 --validate --timing --comm-mode PS --gpu 0 &
-python ${mainpy} --model $1 --dataset $2 --validate --timing --comm-mode PS --gpu 1 &
-wait
+heturun -s 1 -w 2 python ${mainpy} --model $1 --dataset $2 --validate --timing --comm-mode PS
--- examples/cnn/scripts/hetu_8gpu.sh
+++ examples/cnn/scripts/hetu_8gpu.sh
@@ -4,5 +4,4 @@ mainpy=${workdir}/../main.py
 depsdir=${workdir}/../../..
 
 ### validate and timing
-# 
-NCCL_DEBUG=INFO mpirun --allow-run-as-root -np 8 -x PYTHONPATH=/home/public/third_party_tests/Athena/python /root/anaconda3/envs/zhl/bin/python ${mainpy} --model $1 --dataset $2 --learning-rate 0.00125 --validate --timing --comm-mode AllReduce
+heturun -w 8 python ${mainpy} --model $1 --dataset $2 --learning-rate 0.00125 --validate --timing --comm-mode AllReduce
