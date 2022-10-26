765254cf200d6ef36d85ba24093104197191859d
Hankpipi <77650497+Hankpipi@users.noreply.github.com>
2022-10-26 21:49:56 +0800

    fix sparse push block and update readme (#43)

---
+++ examples/nlp/bert/scripts/train_hetu_bert_large_ps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+heturun -s 1 -w 4 python train_hetu_bert_ps.py \
+--train_batch_size 4 \
+--dataset wikicorpus_en \
+--vocab_size 30522 \
+--hidden_size 1024 \
+--num_hidden_layers 24 \
+--num_attention_heads 16 \
+--seq_length 128 \
+--epochs 20 \
+--lr 1e-5 \
+--adam_weight_decay 0.01 \
+--hidden_act relu \
+--dropout_prob 0.1
