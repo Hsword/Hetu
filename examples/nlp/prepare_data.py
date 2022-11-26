50c1ff111cdeeed67cd1dd8d1b3568700d24aca0
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 17:40:23 +0000

    Adding tarfile member sanitization to extractall()

--- examples/nlp/prepare_data.py
+++ examples/nlp/prepare_data.py
@@ -117,7 +117,26 @@ if __name__ == '__main__':
     if not os.path.exists('de-en'):
         print('Extracting iwslt2016...')
         with tarfile.open(file_name) as tar:
-            tar.extractall('./')
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, "./")
 
     os.chdir('../')
     hparams = Hparams()
